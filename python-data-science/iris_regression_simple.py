80a9389ef86271af4db2683a18d2cdccb8f486de
Marko Knoebl <marko.kn@gmail.com>
2019-07-28 16:53:34 +0200

    Adding python exercises

---
+++ python-data-science/iris_regression_simple.py
@@ -0,0 +1,13 @@
+from sklearn.linear_model import LinearRegression
+
+from sklearn.datasets import load_iris
+
+dataset = load_iris().data[:50]
+X = dataset[:, 0:1]
+y = dataset[:, 1]
+
+regr = LinearRegression()
+regr.fit(X, y)
+
+prediction = regr.predict([[4.5], [5.0], [5.5]])
+print(prediction)
